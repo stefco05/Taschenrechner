0ce0d6358f4c2c7640f434c446471ac50247aadc
Stefano Cocco <s_cocco@icloud.com>
2021-03-15 13:26:49 +0100

    Initial Commit

---
+++ Taschenrechner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Taschenrechner
+//
+//  Created by Stefano Cocco on 15.03.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Taschenrechner/TaschenrechnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaschenrechnerApp.swift
+//  Taschenrechner
+//
+//  Created by Stefano Cocco on 15.03.21.
+//
+
+import SwiftUI
+
+@main
+struct TaschenrechnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
